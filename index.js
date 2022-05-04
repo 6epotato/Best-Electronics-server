0bc33dad3af2edc0a49b4d8324066afc79f7fa9e
6epotato <aarifurrahman93@gmail.com>
2022-05-04 22:37:23 +0600

    server created commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+const port = process.env.port || 5000;
+
+// middle ware 
+
+app.use(cors());
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send("Running best electronic server")
+})
+
+
+app.listen(port, () => {
+    console.log('listining port ', port);
+})
