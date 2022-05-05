24124becd30b50c0ecc89681b333c7f6c8db9bc6
6epotato <aarifurrahman93@gmail.com>
2022-05-05 15:02:01 +0600

    delete done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.port || 5000;
@@ -8,6 +9,58 @@ const port = process.env.port || 5000;
 
 app.use(cors());
 app.use(express.json())
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sfvtb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db('BestElectronics').collection('item');
+
+        // find all items
+        app.get('/item', async (req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        })
+
+        // find a single id 
+
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const item = await itemCollection.findOne(query);
+            res.send(item);
+        })
+
+        //Post 
+        app.post('/item', async (req, res) => {
+            const newItem = req.body;
+            const result = await itemCollection.insertOne(newItem);
+            res.send(result);
+        })
+
+        //delete 
+
+        app.delete('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemCollection.deleteOne(query);
+            res.send(result);
+        });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
