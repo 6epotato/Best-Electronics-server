db168957d9cc84f115389b02d9bfc740506f217a
6epotato <aarifurrahman93@gmail.com>
2022-05-08 00:14:32 +0600

    solving

--- index.js
+++ index.js
@@ -16,15 +16,12 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sfvtb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
         await client.connect();
         const itemCollection = client.db('BestElectronics').collection('item');
         const emailCollection = client.db('BestElectronics').collection('email');
-
-        console.log("all routes working")
 
         // find all items
         app.get('/item', async (req, res) => {
