a8dcb993ab16aac07ee184eff07f705b5b624ecc
arifur-rahman1 <aarifurrahman93@gmail.com>
2022-12-26 13:27:58 +0600

    update pass

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sfvtb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://dbuser1:Qam1apRxGV138W8N@cluster0.sfvtb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
